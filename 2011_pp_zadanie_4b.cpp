4ac7fc229fb86f6bb808003f71e57d01da638e33
slo1c292021 <81639902+slo1c292021@users.noreply.github.com>
2022-03-24 10:19:45 +0100

    Create 2011_pp_zadanie_4b.cpp

---
+++ 2011_pp_zadanie_4b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool is_palindrome(string s)
+{
+    int p, k;
+    p = 0;
+    k = s.size()-1;
+    while(p<k){
+
+           if(s[p] != s[k]) return false;
+           p++;
+           k--;
+       }
+        return true;
+}
+int main()
+{
+      int licznik = 0;
+      string s;
+      ifstream f("hasla.txt");
+      while(f>>s)
+      {
+          if(is_palindrome(s))
+          {
+           licznik++;
+           cout << s << "\n";
+          }
+
+      }
+      cout << "Wynik = " << licznik << "\n";
+}
